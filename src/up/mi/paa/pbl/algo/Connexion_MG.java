95c47a9fa26d29431e17dfe6687df4a7ebde9888
Zalcsibalcsi <35973484+Zalcsibalcsi@users.noreply.github.com>
2025-10-29 15:59:52 +0100

    Updating of Connexion_MG
    Connexion_MG a maintenant un static hashSet, qui permet de faire des mis-à-jours.
    
    Il est possible de conserver une référence a une connexion_MG qui n'est plus dans le hashSet. Je ne sais pas comment régler ça.

--- src/up/mi/paa/pbl/algo/Connexion_MG.java
+++ src/up/mi/paa/pbl/algo/Connexion_MG.java
@@ -1,12 +1,16 @@
 package up.mi.paa.pbl.algo;
 
+import java.util.HashSet;
+
 public class Connexion_MG {
 	private Maison maison;
 	private Generateur generateur;
+	private static HashSet<Connexion_MG> setConnexion_MG = new HashSet<Connexion_MG>();
 	
 	public Connexion_MG(Maison maison, Generateur generateur) {
 		this.maison = maison;
 		this.generateur = generateur;
+		Connexion_MG.setConnexion_MG.add(this);
 	}
 	
 	@Override
@@ -19,8 +23,13 @@ public class Connexion_MG {
 		}
 	}
 	
-	//TODO HashCode in a similar fashion
-
+	@Override
+	public int hashCode() {
+		return (251*this.maison.hashCode())+this.generateur.hashCode();
+		/*Il y aura des collisions, mais il n'est pas nécessaire de les éviter.
+		 * La multiplication avec un premier évite juste ("M", "G").hashCode == ("G", "M").hashCode, où les deux paires sont (instanceof Maison, instanceof Generateur)...*/
+	}
+	
 	public Maison getMaison() {
 		return maison;
 	}
@@ -28,4 +37,21 @@ public class Connexion_MG {
 	public Generateur getGenerateur() {
 		return generateur;
 	}
+	
+	public void update(Generateur generateur) {
+		this.update(this.maison, generateur);
+	}
+	
+	public void update(Maison maison) {
+		this.update(maison, this.generateur);
+	}
+
+	public void update(Maison maison, Generateur generateur) {
+		Connexion_MG.setConnexion_MG.remove(this);
+		this.generateur = generateur;
+		this.maison = maison;
+		Connexion_MG.setConnexion_MG.add(this);
+	}
+	/*TODO Il est possible de conserver une référence a une connexion_MG qui n'est plus dans le hashSet. 
+	 * 	   Je ne sais pas comment régler ça.*/
 }

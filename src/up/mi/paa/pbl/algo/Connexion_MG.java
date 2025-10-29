ecc0bc4787ee18a064378a939637af44f95016c6
Zalcsibalcsi <35973484+Zalcsibalcsi@users.noreply.github.com>
2025-10-29 14:31:06 +0100

    Populated Reseau, Connexion_MG
    Instanciation de base de Reseau et de Connexion_MG.
    
    Supprimation du fichier de test de commit.

--- src/up/mi/paa/pbl/algo/Connexion_MG.java
+++ src/up/mi/paa/pbl/algo/Connexion_MG.java
@@ -1,5 +1,31 @@
 package up.mi.paa.pbl.algo;
 
 public class Connexion_MG {
+	private Maison maison;
+	private Generateur generateur;
+	
+	public Connexion_MG(Maison maison, Generateur generateur) {
+		this.maison = maison;
+		this.generateur = generateur;
+	}
+	
+	@Override
+	public boolean equals(Object other) {
+		if (!(other instanceof Connexion_MG)) {
+			return Boolean.FALSE;
+		}
+		else {
+			return (this.maison.equals(((Connexion_MG) other).getMaison()) && this.generateur.equals(((Connexion_MG) other).getGenerateur()));
+		}
+	}
+	
+	//TODO HashCode in a similar fashion
 
+	public Maison getMaison() {
+		return maison;
+	}
+
+	public Generateur getGenerateur() {
+		return generateur;
+	}
 }

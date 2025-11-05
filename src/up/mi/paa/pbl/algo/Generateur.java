dd3b66bd7272c7535c1dfdf6f384a416664ac798
Zalcsibalcsi <35973484+Zalcsibalcsi@users.noreply.github.com>
2025-11-05 15:08:13 +0100

    Résolution des conflits comme agréé en group
    Conflits résolue comme agréé dans le groupe.
    
    Je me suis permis de corriger getCapaciteMAx pour Max. (choix)
    
    Enlevé les imports non-utilisés. (choix)
    
    Ajouté un throw au ajouterConnexion, pour pouvoir respecter "message d’erreur si ce n’est pas le cas" (pdf page 4) (csq du merge)

--- src/up/mi/paa/pbl/algo/Generateur.java
+++ src/up/mi/paa/pbl/algo/Generateur.java
@@ -1,23 +1,13 @@
 package up.mi.paa.pbl.algo;
 
-import java.util.HashMap;
 
-<<<<<<< HEAD
+public class Generateur {
 	private String nom;
 	private int capaciteMax;
-	
 	
 	public Generateur(String n, int val) {
 		this.nom = n;
 		this.capaciteMax = val;
-=======
-public class Generateur {
-	private String nom;
-	private static HashMap<Generateur, Integer> mapGenerateursCapacites = new HashMap<Generateur, Integer>();
-	
-	public Generateur(String nom, int capacite) {
-		this.nom = nom;
-		Generateur.mapGenerateursCapacites.put(this, capacite);	//mettre à jour la valeur dans le HashMap ou ajoute le paire le cas échéant.
 	}
 	
 	@Override
@@ -33,15 +23,13 @@ public class Generateur {
 	@Override
 	public int hashCode() {
 		return this.nom.hashCode();
->>>>>>> master
 	}
 	
 	public String getNom() {
 		return this.nom;
 	}
 	
-<<<<<<< HEAD
-	public int getCapaciteMAx() {
+	public int getCapaciteMax() {
 		return this.capaciteMax;
 	}
 	
@@ -50,15 +38,8 @@ public class Generateur {
 	}
 	
 	
-}
-=======
-	public int getCapacite() {
-		return Generateur.mapGenerateursCapacites.get(this);
-	}
-	
 	@Override
     public String toString() {
-        return "Générateur " + nom + " (Capacité: " + this.getCapacite() + "kWh)";
+        return "Générateur " + nom + " (Capacité: " + this.getCapaciteMax() + "kWh)";
     }
 }
->>>>>>> master
--- src/up/mi/paa/pbl/algo/Maison.java
+++ src/up/mi/paa/pbl/algo/Maison.java
@@ -1,48 +1,7 @@
 package up.mi.paa.pbl.algo;
 
-<<<<<<< HEAD
-import up.mi.paa.problem.algo.TypeConsommation;
-=======
-import java.util.HashMap;
+import up.mi.paa.pbl.algo.TypeConsommation;	
 
-public class Maison {
-	private String nom;
-	private static HashMap<Maison, Maison_Consommation> mapMaisonsConsommations = new  HashMap<Maison, Maison_Consommation>();
-	
-	public Maison(String nom, Maison_Consommation consommation) {
-		this.nom = nom;
-		Maison.mapMaisonsConsommations.put(this, consommation);	//mettre à jour la valeur dans le HashMap ou ajoute le paire le cas échéant.
-	}
-
-	@Override
-	public boolean equals(Object other) {
-		if (!(other instanceof Maison)) {
-			return Boolean.FALSE;
-		} else {
-			return this.nom.equals(((Maison) other).getNom()); //la maison est entièrement identifié par son nom.
-		}
-	}
-	
-	@Override
-	public int hashCode() {
-		return this.nom.hashCode(); //la maison est entièrement identifié par son nom.
-	}
-
-	public String getNom() {
-		return nom;
-	}
-	
-	public Maison_Consommation getConsommation() {
-		return Maison.mapMaisonsConsommations.get(this);
-		/*Comme ça même si deux instances de Maison (avec des nom égaux) sont des objets différents,
-		 *		leur consommation est forcé d'être la même et de suivre les m-à-j dynamiquement.*/
-	}
-	
-	@Override
-    public String toString() {
-        return "Maison " + nom + " (Consommation: " + this.getConsommation().name() + ", " + this.getConsommation().getkWh() + "kWh)";
-    }
->>>>>>> master
 
 public class Maison {
 	private String nom;
@@ -69,20 +28,24 @@ public class Maison {
 		this.type = t;
 	}
 	
+	@Override
+	public boolean equals(Object other) {
+		if (!(other instanceof Maison)) {
+			return Boolean.FALSE;
+		} else {
+			return this.nom.equals(((Maison) other).getNom()); //la maison est entièrement identifié par son nom.
+		}
+	}
 	
-}
+	@Override
+	public int hashCode() {
+		return this.nom.hashCode(); //la maison est entièrement identifié par son nom.
+	}
 
-/* friendly */ enum Maison_Consommation { // Mon compilateur a rejeté le friendly exprès.
-	BASSE(10), NORMALE(20), FORTE(40);
 	
-	private int kWh;	//Je pense à faire << un_maison.getConsommation().getkWh() >> plus tard, qui me semble assez raisonnable comme phrase.
+	@Override
+    public String toString() {
+        return "Maison " + nom + " (Consommation: " + this.getType().name() + ", " + this.getType().getDemande() + "kWh)";
+    }
 	
-	private Maison_Consommation(int kWh) {
-		this.kWh = kWh;
-	}
-	
-	public int getkWh() {
-		/*Je pense à faire << un_maison.getConsommation().getkWh() >> plus tard, qui me semble assez raisonnable comme phrase. */
-		return this.kWh;
-	}
 }

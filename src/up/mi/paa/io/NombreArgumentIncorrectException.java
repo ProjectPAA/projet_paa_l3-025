f836563ea1a921d9283069d15f9b445b58add7dc
Zalcsibalcsi <35973484+Zalcsibalcsi@users.noreply.github.com>
2025-12-14 17:22:41 +0100

    Added Doc for io.*Exception

--- src/up/mi/paa/io/NombreArgumentIncorrectException.java
+++ src/up/mi/paa/io/NombreArgumentIncorrectException.java
@@ -1,7 +1,19 @@
 package up.mi.paa.io;
 
 
+/**
+ * Exception signalant que le nombre d'arguments dans une ligne de {@linkplain java.io.File fichier} est incorrect (différent de deux).
+ * @see ChargeurReseau#traiterLigne(String, Reseau)
+ * @author Jacques ZHENG
+ * @author Mamadou NIMAGA DIT
+ * @author Zalán MOLNÁR
+ */
 public class NombreArgumentIncorrectException extends Exception{
+	/**
+	 * Exception signalant que le nombre d'arguments dans une ligne de {@link java.io.File fichier} est incorrect.
+	 * @see ChargeurReseau#traiterLigne(String, Reseau)
+	 * @param message Texte de l'exception
+	 */
 	public NombreArgumentIncorrectException(String message) {
 		super(message);
 	}

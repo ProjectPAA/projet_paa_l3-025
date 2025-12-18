9bb781402b1c41cff27fb35fe22b3334e0733c41
Zalcsibalcsi <35973484+Zalcsibalcsi@users.noreply.github.com>
2025-12-18 18:03:10 +0100

    Fixed typo

--- src/up/mi/paa/inter_face/InterfaceTextuelle.java
+++ src/up/mi/paa/inter_face/InterfaceTextuelle.java
@@ -518,7 +518,7 @@ public class InterfaceTextuelle {
 		double coutApres = network.calculerCout(lambda);
 
 		System.out.println("\n--- RESULTATS DE L'OPTIMISATION ---");
-		System.out.println("Algorithme temriné en " + (endTime - startTime) + " ms.");
+		System.out.println("Algorithme terminé en " + (endTime - startTime) + " ms.");
 		System.out.println("Coût AVANT : " + String.format("%.4f", coutAvant));
 		System.out.println("Coût APRES : " + String.format("%.4f", coutApres));
 
